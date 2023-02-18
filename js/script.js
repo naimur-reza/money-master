c9630382138905db54499f9163d6c4f146a7ea5a
Naimur Reza <95697060+naimur-reza@users.noreply.github.com>
2023-02-18 14:55:39 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const foodValue = getValueFromInput("food");
   const clothesValue = getValueFromInput("clothes");
   const billsTotal = getValueFromInput("bill");
+  if(isNaN(totalValue) || isNaN(foodValue) || isNaN(clothesValue)){
+    alert("Please provide valid number")
+    return;
+  }
   const totalCost = foodValue + clothesValue + billsTotal;
   const restTotal = totalValue - totalCost;
   setValue("total-expense", totalCost);
@@ -28,11 +32,16 @@ document.getElementById("saving-btn").addEventListener("click", function () {
   const savingPercentage = getValueFromInput("save");
   const totalValue = getValueFromInput("income");
   //get expense balance
+  if(isNaN(savingPercentage) || isNaN(totalValue)){
+    alert("Enter a valid number");
+    return;
+  }
   const expenseBalanceId = document.getElementById('total-expense');
   const expenseBalanceString = expenseBalanceId.innerText;
   const expenseBalance = parseFloat(expenseBalanceString);
-  const saveTotal = (savingPercentage / 100) * totalValue;
-  const remainingBalance = expenseBalance - saveTotal;
+  const saveTotal = ((savingPercentage / 100) * expenseBalance).toFixed(2);
+  const remainingBalance = (expenseBalance - saveTotal).toFixed(2);
   setValue("saving-amount", saveTotal);
-  setValue ('remaining-balance', remainingBalance);
+  setValue ("remaining-balance", remainingBalance);
+  console.log(remainingBalance);
 });
